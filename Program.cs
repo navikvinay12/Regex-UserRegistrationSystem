d0079fbec6c9716077b9572a6cf5fc18da888bcf
Vinay <navik.vinay12@gmail.com>
2022-12-01 20:20:06 +0530

    first name registration/validation

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace UserRegistrationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration using Regex.\n");
+            while(true)
+            {
+                Console.WriteLine("Please select any one from below :\n" +
+                "Press 1 : To Enter your First Name.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UserDetails.FirstName();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option selected ,Please try again !");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationSystem
+{
+    public class UserDetails
+    {
+        public static void FirstName()      //UC1
+        {
+            Console.WriteLine("Enter your FirstName");
+            string userInput = Console.ReadLine();
+            string regexCondition = "^[A-Z]{1}[a-z]{3,}$";
+            Iteration(userInput, regexCondition);
+        }
+        public static void Iteration(string userInput, string regexCondition)
+        {
+            if (Regex.IsMatch(userInput,regexCondition))
+            {
+                Console.WriteLine("Validated successfully!\n");
+            }
+            else
+            {
+                Console.WriteLine("Entered Details are not in required format.Please try again!\n");
+            }
+        }
+    }
+}
